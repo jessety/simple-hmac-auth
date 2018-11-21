e254f5660572e9f519fa9a2825b76aef1a42d84c
Jesse <jessetyoungblood@gmail.com>
2018-11-21 17:36:25 -0500

    Prototyping

---
+++ index.js
@@ -0,0 +1,10 @@
+//
+//  Index.js
+//  Created by Jesse T Youngblood on 11/21/18 at 15:19am 
+//    
+
+'use strict';
+
+const EasySigning = require('./EasySigning');
+
+module.exports.EasySigning = EasySigning;
